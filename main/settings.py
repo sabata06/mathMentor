09c57e5fb69ccc384d9f55ea400a65ba002ef7c6
sabata06 <m.karaarslan06@outlook.com>
2025-01-13 00:37:53 +0300

    update database settings to use dj-database-url for production; add required packages to requirements.txt

--- main/settings.py
+++ main/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,10 +56,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_USER_MODEL = 'mathmentor.CustomUser'
